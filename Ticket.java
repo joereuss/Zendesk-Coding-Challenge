38060cda522fc9a45362420a74de468ca0e49e54
Joseph Reuss <71942931+joereuss12@users.noreply.github.com>
2021-07-30 11:30:17 -0400

    Update Ticket.java
    Should be the finished Ticket.java, just need to add comments and probably better exception message

--- Ticket.java
+++ Ticket.java
@@ -1,55 +1,45 @@
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONArray;
 
-/*
+
+/**
  * This class is to represent the Ticket object
+ * 
+ * @author Joseph Reuss
+ *
  */
 public class Ticket {
 
-  private JSONArray jarray;
   JSONObject jobject;
   private String subject;
-  private String ID = "0";
-  private String description = "";
+  private int ID;
+  private String description;
+  private String createdTime;
 
 
   public Ticket(String ticket) {
 
-//    try {
-//      jobject = new JSONObject(ticket);
-//      jarray = jobject.getJSONArray("tickets");
-//      for (int i = 0; i < jarray.length(); i++) {
-//        subject = jarray.getJSONObject(i).getString("subject");
-//        ID = jarray.getJSONObject(i).getString("ID");
-//        description = jarray.getJSONObject(i).getString("description");
-//        
-//      }
-//    } catch (JSONException e) {
-//      // TODO Auto-generated catch block
-//      e.printStackTrace();
-//    }
-
-
 
      try {
+     
      jobject = new JSONObject(ticket);
      subject = jobject.getJSONObject("ticket").getString("subject");
-     ID = jobject.getJSONObject("ticket").getString("id");
+     ID = jobject.getJSONObject("ticket").getInt("id");
      description = jobject.getJSONObject("ticket").getString("description");
      } catch (JSONException e) {
-     // TODO Auto-generated catch block
      System.out.println("not a valid json object");
      e.printStackTrace();
      }
+    
 
   }
+
 
   public String getSubject() {
     return subject;
   }
 
-  public String getID() {
+  public int getID() {
     return ID;
   }
 
