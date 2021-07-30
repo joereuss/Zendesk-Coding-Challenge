0be94523aa1df018a0af41ff6f417c16503a6eac
Joseph Reuss <71942931+joereuss12@users.noreply.github.com>
2021-07-30 11:59:44 -0400

    Update Ticket.java
    Added comments to the code

--- Ticket.java
+++ Ticket.java
@@ -17,31 +17,47 @@ public class Ticket {
   private String createdTime;
 
 
+  /**
+   * This method represents the Ticket object constructor
+   * 
+   * @param ticket - the json string passed to the ticket constructor
+   */
   public Ticket(String ticket) {
+    try {
+      jobject = new JSONObject(ticket);
+      subject = jobject.getJSONObject("ticket").getString("subject");
+      ID = jobject.getJSONObject("ticket").getInt("id");
+      description = jobject.getJSONObject("ticket").getString("description");
+    } catch (JSONException e) {
+      System.out.println("not a valid json object");
+      e.printStackTrace();
+    }
 
-
-     try {
-     
-     jobject = new JSONObject(ticket);
-     subject = jobject.getJSONObject("ticket").getString("subject");
-     ID = jobject.getJSONObject("ticket").getInt("id");
-     description = jobject.getJSONObject("ticket").getString("description");
-     } catch (JSONException e) {
-     System.out.println("not a valid json object");
-     e.printStackTrace();
-     }
-    
 
   }
 
 
+  /**
+   * Getter for the String subject
+   * @return - subject
+   */
   public String getSubject() {
     return subject;
   }
 
+  /** 
+   * Getter for the int ID
+   * @return - ID
+   */
+
   public int getID() {
     return ID;
   }
+
+  /**
+   * Getter for the String description
+   * @return - description
+   */
 
   public String getDescription() {
     return description;
